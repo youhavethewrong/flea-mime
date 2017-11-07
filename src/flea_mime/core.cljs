fb4fc3ba753073ac6386b5c761834ab06ef3136d
ESC <youhavethewrong@gmail.com>
2017-11-07 16:51:10 -0600

    Test out repl with a node dep.

--- src/flea_mime/core.cljs
+++ src/flea_mime/core.cljs
@@ -1,9 +1,16 @@
 (ns flea-mime.core
   (:require [cljs.nodejs :as node]
+            [clojure.pprint :as pprint]
             [gpio.core :as gpio]))
 
 (node/enable-util-print!)
 (.on js/process "uncaughtException" #(js/console.error %))
+
+(defonce fs (node/require "fs"))
+
+(defn eat-tacos
+  [filename]
+  (.writeFileSync fs filename (pprint/write {:tacos "delicious"} :pretty true :stream nil)))
 
 (defn -main
   [& args]
